b632b6450585a094002fbf430577f65a4d7e861c
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-20 14:36:53 +0200

    Updated Customer model with its fields

--- Booking_System.Models/Customer.cs
+++ Booking_System.Models/Customer.cs
@@ -1,7 +1,23 @@
-﻿namespace Booking_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Booking_System.Models
 {
     public class Customer
     {
+        public int Id { get; set; }
 
+        [MinLength(2, ErrorMessage = "Minimum of 2 characters is required")]
+        [MaxLength(50, ErrorMessage = "Maximum of 50 characters")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [MinLength(2, ErrorMessage = "Minimum of 2 characters is required")]
+        [MaxLength(50, ErrorMessage = "Maximum of 50 characters")]
+        public string LastName { get; set; } = string.Empty;
+
+        [EmailAddress(ErrorMessage = "Invalid Email format")]
+        public string EmailAddress { get; set; } string.Empty;
+
+        [Phone(ErrorMessage = "Invalid PhoneNumber format")]
+        public string PhoneNumber { get; set; } string.Empty;
     }
 }
