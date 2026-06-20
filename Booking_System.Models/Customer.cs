f0552f0070145f177c89e16ac6caa1cbfdb477df
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-20 16:24:43 +0200

    Added Booking list on Customers

--- Booking_System.Models/Customer.cs
+++ Booking_System.Models/Customer.cs
@@ -6,18 +6,24 @@ namespace Booking_System.Models
     {
         public int Id { get; set; }
 
+        [Required]
         [MinLength(2, ErrorMessage = "Minimum of 2 characters is required")]
         [MaxLength(50, ErrorMessage = "Maximum of 50 characters")]
         public string FirstName { get; set; } = string.Empty;
 
+        [Required]
         [MinLength(2, ErrorMessage = "Minimum of 2 characters is required")]
         [MaxLength(50, ErrorMessage = "Maximum of 50 characters")]
         public string LastName { get; set; } = string.Empty;
 
+        [Required]
         [EmailAddress(ErrorMessage = "Invalid Email format")]
-        public string EmailAddress { get; set; } string.Empty;
+        public string EmailAddress { get; set; } = string.Empty;
 
+        [Required]
         [Phone(ErrorMessage = "Invalid PhoneNumber format")]
-        public string PhoneNumber { get; set; } string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
     }
 }
