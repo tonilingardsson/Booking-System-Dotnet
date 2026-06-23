4f1c7ac23607332557600ea7bb4af401067b401d
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-23 09:11:56 +0200

    Seed initial data for Customers so I can test Post without issues

---
+++ Booking_System.Api/Migrations/20260623060745_SeedTestData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booking_System_Dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedTestData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "Id", "EmailAddress", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[] { 1, "antonio@luna.com", "Antonio", "Luna", "0729291305" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
