f0ebc41bc850f5be4f7ce0cd907d8df578d87943
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-07-07 15:58:54 +0200

    Add POST, UPDATE and DELETE methods to ICustomerService

--- Booking_System.Api/Services/ICustomerService.cs
+++ Booking_System.Api/Services/ICustomerService.cs
@@ -8,5 +8,10 @@ namespace Booking_System.Api.Services
         Task<IEnumerable<Customer>> GetAllCustomersAsync();
 
         Task<Customer?> GetCustomerByIdAsync(int id);
+        // We don't need the id because it is created with the customer object
+        Task<Customer?> CreateCustomerAsync(Customer customer);
+        Task<Customer?> UpdateCustomerAsync(int id, Customer customer);
+        // We don't need customer object because it is going to be deleted using the id
+        Task<Customer?> DeleteCustomerAsync(int id);
     }
 }
