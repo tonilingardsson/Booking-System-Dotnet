44cb6a8c9f2018b09a7042facb7d6f067f83e85e
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-07-24 18:00:16 +0200

    Added CustomerValidationTests using MSTest to verify that creating a customer with a duplicate email throws CustomerValidationException.

---
+++ Booking_System.Tests/CustomerValidationTests.cs
@@ -0,0 +1,58 @@
+﻿using Booking_System.Api.Data;
+using Booking_System.Api.Services;
+using Booking_System.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Booking_System.Tests
+{
+    [TestClass]
+    public class CustomerValidationTests
+    {
+        private BookingDbContext _context = null!;
+        private CustomerService _service = null!;
+        
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<BookingDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            
+            _context = new BookingDbContext(options);
+           
+            _context.Customers.Add(new Customer
+            {
+                Id = 1,
+                FirstName = "Antonio",
+                LastName = "Luna",
+                EmailAddress = "antonio@luna.com",
+                PhoneNumber = "0729291305"
+            });
+
+            _context.SaveChanges();
+            _service = new CustomerService(_context);
+        }
+
+        [TestMethod]
+        public async Task GetAllCustomersAsync_ReturnsCustomers()
+        {
+            // Arrange
+            var customer = new Customer
+            {
+                Id = 2,
+                FirstName = "Toni",
+                LastName = "Luna",
+                EmailAddress = "antonio@luna.com",
+                PhoneNumber = "0701234567"
+            };
+
+            // Act & Assert
+
+             var exception = await Assert.ThrowsAsync<CustomerValidationException>(() => _service.CreateCustomerAsync(customer));
+            // It should return the first customer in the database, which is Antonio Luna
+            // and the second customer should not be added due to the duplicate email address
+            Assert.AreEqual("A customer with the same email address already exists.", exception.Message);
+        }
+    }
+}
