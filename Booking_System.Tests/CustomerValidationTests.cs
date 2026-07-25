dd8f0fdfde2cbe9589a27ae3cd81bf5aac254547
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-07-25 18:21:14 +0200

    Added GetCustomerByIdAsync_ExistingId_ReturnsCustomer unit test to CustomerValidationTests.

--- Booking_System.Tests/CustomerValidationTests.cs
+++ Booking_System.Tests/CustomerValidationTests.cs
@@ -11,16 +11,16 @@ namespace Booking_System.Tests
     {
         private BookingDbContext _context = null!;
         private CustomerService _service = null!;
-        
+
         [TestInitialize]
         public void Setup()
         {
             var options = new DbContextOptionsBuilder<BookingDbContext>()
                 .UseInMemoryDatabase(Guid.NewGuid().ToString())
                 .Options;
-            
+
             _context = new BookingDbContext(options);
-           
+
             _context.Customers.Add(new Customer
             {
                 Id = 1,
@@ -49,10 +49,36 @@ namespace Booking_System.Tests
 
             // Act & Assert
 
-             var exception = await Assert.ThrowsAsync<CustomerValidationException>(() => _service.CreateCustomerAsync(customer));
+            var exception = await Assert.ThrowsAsync<CustomerValidationException>(() => _service.CreateCustomerAsync(customer));
             // It should return the first customer in the database, which is Antonio Luna
             // and the second customer should not be added due to the duplicate email address
             Assert.AreEqual("A customer with the same email address already exists.", exception.Message);
         }
+
+        [TestMethod]
+        public async Task GetCustomerByIdAsync_ExistingId_ReturnsCustomer()
+        {
+            // Arrange
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Antonio",
+                LastName = "Luna",
+                EmailAddress = "antonio@luna.com",
+                PhoneNumber = "0729291305"
+            };
+
+            // Act
+            var result = await _service.GetCustomerByIdAsync(customer.Id);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(customer.Id, result.Id);
+            Assert.AreEqual(customer.FirstName, result.FirstName);
+            Assert.AreEqual(customer.LastName, result.LastName);
+            Assert.AreEqual(customer.EmailAddress, result.EmailAddress);
+            Assert.AreEqual(customer.PhoneNumber, result.PhoneNumber);
+        }
+
     }
 }
