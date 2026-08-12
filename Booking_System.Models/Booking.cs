beb1fdbb842c6fbb74e3d86c151acec590a72298
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-08-12 14:42:10 +0200

    Refactored the Booking class to remove unnecessary nesting and namespace.

--- Booking_System.Models/Booking.cs
+++ Booking_System.Models/Booking.cs
@@ -1,20 +1,15 @@
-﻿using System;
+﻿using Booking_System.Models;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Booking_System.Models
+public class Booking
 {
-    public class Booking
-    {
-        public int Id { get; set; }
-        
-        public int CustomerId { get; set; }
-        
-        public Customer? Customer { get; set; } = null!;
-        
-        public int CourtId { get; set; }
-        
-        public Court? Court { get; set; } = null!;
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public int CourtId { get; set; }
+    public DateTime StartTime { get; set; }
 
-        public DateTime StartTime {  get; set; }
-        public DateTime EndTime { get; set; }
-    }
+    [NotMapped] // EF do not look for this property in the database, it is calculated on the fly
+    public DateTime EndTime => StartTime.AddHours(1);
+    public Customer? Customer { get; set; }
+    public Court? Court { get; set; }
 }
