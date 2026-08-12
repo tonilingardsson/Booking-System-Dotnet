e7e879b15f9ece68e154f23dfaee663dc123c308
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-08-12 16:55:12 +0200

    Seed initial data for Court and Customer entities to enable new endpoints operability

--- Booking_System.Api/Data/BookingDbContext.cs
+++ Booking_System.Api/Data/BookingDbContext.cs
@@ -5,54 +5,38 @@ namespace Booking_System.Api.Data
 {
     public class BookingDbContext : DbContext
     {
-        // Three DbSet properties only
         public DbSet<Booking> Bookings { get; set; }
         public DbSet<Court> Courts { get; set; }
         public DbSet<Customer> Customers { get; set; }
 
-        // Constructor
         public BookingDbContext(DbContextOptions<BookingDbContext> options) : base(options)
-        { }
+        {
+        }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
-
-        //    modelBuilder.Entity<Court>().HasData(
-        //        new Court
-        //        {
-        //            Id = 1,
-        //            CourtName = "Rafa Nadal"
-        //        },
-        //        new Court
-        //        {
-        //            Id = 2,
-        //            CourtName = "Roger Federer"
-        //         },
-        //        new Court
-        //        {
-        //            Id = 3,
-        //            CourtName = "Björn Borg"
-        //        }
-        //    );
-
-        //    modelBuilder.Entity<Customer>().HasData(
-        //        new Customer
-        //        {
-        //            Id = 1,
-        //            FirstName = "Antonio",
-        //            LastName = "Luna",
-        //            EmailAddress = "antonio@luna.com",
-        //            PhoneNumber = "0729291305"
-        //        }
-        //    );
-        //}
-
-        // Create _service = new BookinService(...)
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            // Seed the three courts required by the assignment
+            modelBuilder.Entity<Court>().HasData(
+                new Court
+                {
+                    Id = 1,
+                    CourtName = "Rafa Nadal"
+                },
+                new Court
+                {
+                    Id = 2,
+                    CourtName = "Roger Federer"
+                },
+                new Court
+                {
+                    Id = 3,
+                    CourtName = "Björn Borg"
+                }
+            );
+
+            // Seed a couple of customers for testing bookings
             modelBuilder.Entity<Customer>().HasData(
                 new Customer
                 {
