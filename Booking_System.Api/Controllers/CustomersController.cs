b9585effafb2cf8a9f07ab4b7c3840442c03203e
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-08-12 16:51:13 +0200

    created a guard clause for Customer in case returns null

--- Booking_System.Api/Controllers/CustomersController.cs
+++ Booking_System.Api/Controllers/CustomersController.cs
@@ -42,6 +42,9 @@ namespace Booking_System.Api.Controllers
         {
             var createdCustomer = await _customerService.CreateCustomerAsync(customer);
 
+            if (createdCustomer is null) {
+                return BadRequest(new { message = "Customer could not be created." });
+            }
             return CreatedAtAction(
                 nameof(GetCustomerById),
                 new { id = createdCustomer.Id }, createdCustomer);
