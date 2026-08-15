a9196c15bdfb454069b753067216b44a56a970be
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-08-15 18:38:49 +0200

    Added CourtDto with validation for CourtName property

---
+++ Booking_System.Api/Dtos/CourtDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Booking_System.Dtos
+{
+    public class CourtDto
+    {
+        [Required(ErrorMessage = "Court name is required")]
+        [MinLength(2, ErrorMessage = "Minimum of 2 characters is required")]
+        [MaxLength(50, ErrorMessage = "Maximum of 50 characters")]
+        public string CourtName { get; set; } = string.Empty;
+    }
+}
